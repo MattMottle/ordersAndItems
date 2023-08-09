1a32145dc610ecea86093ae33fa7edf870e71b78
Matt Mottle <mattmottle1@gmail.com>
2023-08-09 11:56:49 -0700

    Added class methods and constructors

--- Order.java
+++ Order.java
@@ -1,8 +1,69 @@
 import java.util.ArrayList;
 
 class Order {
-    String name;
-    double total;
-    boolean ready;
-    ArrayList<Item> items;
+// member variables
+    private String name;
+    private boolean ready;
+    private ArrayList<Item> items;
+// constructor
+    public Order() {
+        this.name = "Guest";
+        this.items = new ArrayList<Item>();
+    }
+// overloaded constructor
+    public Order(String name) {
+        this.name = name;
+        this.items = new ArrayList<Item>();
+    }
+// order methods
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public String getStatusMessage() {
+        if(this.ready) {
+            return "Your order is ready.";
+        }
+        else {
+            return "Thank you for waiting. Your order will be ready soon.";
+        }
+    }
+
+    public double getOrderTotal() {
+        double total = 0;
+        for(Item item : items) {
+            total += item.getPrice();
+        }
+        return total;
+    }
+    public void display(){
+        System.out.println("Customer Name: " + this.name);
+        for(Item item : items) {
+            System.out.printf("%s - $%.2f \n", item.getName(), item.getPrice());
+        }
+        System.out.printf("Total: $%.2f \n", getOrderTotal());
+    }
+
+// getters and setters
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean ready() {
+        return ready;
+    }
+    public void setReady(boolean ready) {
+        this.ready = ready;
+    }
+
+    public ArrayList<Item> getItems() {
+        return this.items;
+    }
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+    }
 }
+
--- TestOrders.java
+++ TestOrders.java
@@ -1,82 +1,58 @@
-import java.util.ArrayList;
-
 public class TestOrders {
-    public static void main(String[] args) {
-        //Menu items
-        Item item1 = new Item();
-        item1.name = "mocha";
-        item1.price = 2.75;
+    public static void main(String[] args){
 
-        Item item2 = new Item();
-        item2.name = "latte";
-        item2.price = 2.99;
+        // Menu items
+        Item item1 = new Item("drip coffee", 1.60);
+        Item item2 = new Item("capuccino", 2.45);
+        Item item3 = new Item("iced coffee", 2.15);
+        Item item4 = new Item("mocha", 3.10);
 
-        Item item3 = new Item();
-        item3.name = "drip coffee";
-        item3.price = 2.00;
-
-        Item item4 = new Item();
-        item4.name = "cappuccino";
-        item4.price = 2.25;
-
-        //Order variables -- order1, order2 etc.
+        // 2 unspecified orders
         Order order1 = new Order();
-        order1.name = "Cindhuri";
-        order1.total = 0.00;
-        order1.ready = false;
-        order1.items = new ArrayList<Item>();
-
         Order order2 = new Order();
-        order2.name = "Jimmy";
-        order2.total = 0.00;
-        order2.ready = false;
-        order2.items = new ArrayList<Item>();
 
-        Order order3 = new Order();
-        order3.name = "Noah";
-        order3.total = 0.00;
-        order3.ready = false;
-        order3.items = new ArrayList<Item>();
+        // 3 named orders
+        Order order3 = new Order("Matt");
+        Order order4 = new Order("Brad");
+        Order order5 = new Order("Rachel");
 
-        Order order4 = new Order();
-        order4.name = "Sam";
-        order4.total = 0.00;
-        order4.ready = false;
-        order4.items = new ArrayList<Item>();
+        // Add 2 items to each order and call display after
+        order1.addItem(item4);
+        order1.addItem(item4);
 
-        //Application Simulations
-        order2.items.add(item1);
-        order2.total += item1.price;
+        order2.addItem(item1);
+        order2.addItem(item2);
 
-        order3.items.add(item4);
-        order3.total += item4.price;
+        order3.addItem(item4);
+        order3.addItem(item3);
 
-        order4.items.add(item2);
-        order4.total += item2.price;
+        order4.addItem(item2);
+        order4.addItem(item3);
 
-        order1.ready = true;
+        order5.addItem(item1);
+        order5.addItem(item3);
+        order5.addItem(item3);
 
-        order4.items.add(item2);
-        order4.items.add(item2);
-        order4.total += item2.price *2;
+        // Test status
+        System.out.println(order2.getStatusMessage());
+        order2.setReady(true);
+        System.out.println(order2.getStatusMessage());
+
+        System.out.println(order3.getStatusMessage());
+        order3.setReady(true);
+        System.out.println(order3.getStatusMessage());
+
+        System.out.println(order5.getStatusMessage());
+        order5.setReady(true);
+        System.out.println(order5.getStatusMessage());
         
-        order2.ready = true;
+        // Test order total
+        System.out.println(order2.getOrderTotal());
+        order1.display();
+        order2.display();
+        order3.display();
+        order4.display();
+        order5.display();
 
-        //use this example code to test various orders updates
-        System.out.printf("Name: %s\n", order1.name);
-        System.out.printf("Total: %s\n", order1.total);
-        System.out.printf("Ready: %s\n", order1.ready);
-
-        System.out.printf("Name: %s\n", order2.name);
-        System.out.printf("Total: %s\n", order2.total);
-        System.out.printf("Ready: %s\n", order2.ready);
-
-        System.out.printf("Name: %s\n", order3.name);
-        System.out.printf("Total: %s\n", order3.total);
-        System.out.printf("Ready: %s\n", order3.ready);
-
-        System.out.printf("Name: %s\n", order4.name);
-        System.out.printf("Total: %s\n", order4.total);
-        System.out.printf("Ready: %s\n", order4.ready);
     }
 }
